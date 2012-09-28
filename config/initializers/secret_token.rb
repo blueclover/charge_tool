e9762ba297c86f9146946f6eb844b0223bdf43b6
JT <it@cochs.org>
2012-09-28 15:46:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChargeTool::Application.config.secret_token = '08ffe282727816944899adcfeb377e0f0866701372e5911ef29d629d0915a693bf31fbca33aeb3180e50f357ef768b4fbcf046e53ad9c22c28620f8cff8bd737'
